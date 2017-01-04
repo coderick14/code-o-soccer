ace6869deba82dc5daddb9e0910f40f77ba1ff9b
coderick14 <bhattacharyya.rick14@gmail.com>
2017-01-04 21:02:47 +0530

    Initial commit

---
+++ Defender.hpp
@@ -0,0 +1,28 @@
+#pragma once
+#include "skills.h"
+
+namespace MyStrategy
+{
+  // Naive example for defender
+  void defender(BeliefState *state,int botID)
+  {
+    //print("Defender\n");
+	//It follows the ball
+	Vec2D dpoint;
+	dpoint.x = -HALF_FIELD_MAXX / 2;
+	dpoint.y = state->ballPos.y;
+	if (state->ballPos.x < state->homePos[botID].x) {
+		dpoint.x = state->ballPos.x + BOT_BALL_THRESH;
+		dpoint.y = state->ballPos.y;
+		//print("defend");
+	}
+	else if (state->ballPos.x < 0 && state->ballPos.x > state->homePos[botID].x && state->ballPos.x - 3*COLLISION_DIST < state->homePos[botID].x) {
+		//print("shoot");
+		GoToBall(botID, state, true);
+		return;
+	}
+	if (state->pr_ballOurSide)
+		print("%d",state->pr_ballOurSide);
+    GoToPoint(botID,state,dpoint,PI/2,false,false);
+  }
+}
---
+++ GoalKeeper.hpp
@@ -0,0 +1,61 @@
+#pragma once
+#include "skills.h"
+
+/*
+	Vec2D can be used to make variables that can store points as (x,y) and provide utility functions.
+	Vec2D point1;
+	Vec2D point2;
+	point1.x = HALF_FIELD_MAXX;
+	point1.y = 0;
+	similarly for point2 can be specified
+	Some utility functions:
+	These will appear automatically as you type Vec2D::
+	float angle = Vec2D::angle(point1,point2);   
+	int distance = Vec2D::distSq(point1,point2);
+*/
+
+namespace MyStrategy
+{
+	Vec2D homeGoal(OUR_GOAL_X, 0);
+  // Naive Example for GoalKeeper
+  void goalkeeper(BeliefState *state,int botID)
+  {
+	int dist = Vec2D::distSq(state->ballPos, homeGoal);
+	if (dist < DBOX_WIDTH*DBOX_WIDTH * 4) {
+		print("Sparta!!!!!!!!\n");
+		shoot(botID, state, Vector2D<float>(state->ballPos.x, state->ballPos.y));
+		return;
+	}
+	Vec2D pos = state->homePos[botID];
+	if (abs(pos.x - (OUR_GOAL_X + DBOX_HEIGHT) > 220)) {
+		print("GOALKEEPER: MOVE BACK TO BASE %d\t%d\t%d\n", pos.x, OUR_GOAL_X + DBOX_HEIGHT, abs(pos.x - (OUR_GOAL_X + DBOX_HEIGHT)));
+		Vec2D dpoint(OUR_GOAL_X + DBOX_HEIGHT, state->ballPos.y);
+
+		if (dpoint.y > OUR_GOAL_MAXY - 300)
+			dpoint.y = OUR_GOAL_MAXY - 300;
+		if (dpoint.y < OUR_GOAL_MINY + 300)
+			dpoint.y = OUR_GOAL_MINY + 300;
+		GoToPoint(botID, state, dpoint, PI / 2, true, true);
+		return;
+	}
+	if (fabs(state->homeAngle[botID] - PI / 2) > 0.2) {
+		print("Goalkeeper TURNING %f\n", state->homeAngle[botID]);
+		TurnToAngle(botID, state, PI / 2);
+	}
+	else {
+		print("Goalkeeper RUNNING\n");
+		if (state->homePos[botID].y > OUR_GOAL_MAXY) {
+			Velocity(botID, -MAX_BOT_SPEED, -MAX_BOT_SPEED);
+		}
+		else if (state->homePos[botID].y < -OUR_GOAL_MAXY) {
+			Velocity(botID, MAX_BOT_SPEED, MAX_BOT_SPEED);
+		}
+		else if (state->homePos[botID].y > state->ballPos.y) {
+			Velocity(botID, -MAX_BOT_SPEED, -MAX_BOT_SPEED);
+		}
+		else {
+			Velocity(botID, MAX_BOT_SPEED, MAX_BOT_SPEED);
+		}
+	}
+  }
+}
