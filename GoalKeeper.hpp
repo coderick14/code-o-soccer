08fee825f7d127f06d19e6e35bb235d27d871efe
Santa SJ <santhanam1997@gmail.com>
2017-01-25 10:09:32 +0530

    v6.0
    Mode switching for attacker(attacker -> GK)

---
+++ GoalKeeper.hpp
@@ -0,0 +1,54 @@
+#pragma once
+#include "skills.h"
+
+/*
+	Vec2D can be used to make variables that can store points as (x,y) and provide utility functions.
+	Vec2D point1;
+	Vec2D point2;
+	point1.x = HALF_FIELD_MAXX;
+	point1.y = 0;
+	similarly for point2 can be specified
+	Some utility functions:
+	These will appear automatically as you type Vec2D::
+	float angle = Vec2D::angle(point1,point2);
+	int distance = Vec2D::distSq(point1,point2);
+*/
+
+namespace MyStrategy
+{
+	Vec2D homeGoal(-4200, 0);
+
+
+	// Naive Example for GoalKeeper
+	void goalkeeper(BeliefState *state, int botID)
+	{
+		int safeX = -4250;
+		if (botID == 2)
+		{
+			safeX = -3500;
+		}
+		int dist = Vec2D::distSq(state->ballPos, homeGoal);
+		Vec2D safepoint(-4000, 0);
+
+		Vec2D pos = state->homePos[botID];
+		Vec2D dest = Vec2D(pos);
+		Vec2D ballpos = state->ballPos;
+
+		if (dist < DBOX_WIDTH*DBOX_WIDTH * 3) {
+			//print("Shoot!!!");
+			GoToBall(botID, state, false);
+			save_goal(botID, state, Vector2D<float>(state->ballPos.x, state->ballPos.y));
+			return;
+		}
+		dest.y = rayCastY(state, botID);
+		dest.x = safeX;
+		if (dest.y > OUR_GOAL_MAXY) {
+			dest.y = OUR_GOAL_MAXY;
+		}
+		else if (dest.y < OUR_GOAL_MINY) {
+			dest.y = OUR_GOAL_MINY;
+		}
+		//print("RayCast")
+		GoToPoint(botID, state, dest, PI / 2, true, false);
+	}
+}
